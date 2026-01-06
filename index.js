6763e6b497248cd2c367353bae862911ba665d64
chris_ic3 <chris_ic3@hotmail.com>
2026-01-06 11:33:12 -0600

    Add Initial Campground Mongoose Model and Test MongoDB Connectivity

--- index.js
+++ index.js
@@ -1,6 +1,25 @@
 const express = require('express');
-const app = express();
 const path = require('path');
+const mongoose = require('mongoose');
+
+const serverPort = 3000;
+const dbPort = 27017;
+const dbName = 'yelpCamp';
+
+mongoose.connect(`mongodb://localhost:${ dbPort }/${ dbName }`)
+.then(() => {
+    console.log(`MONGODB CONNECTED TO ${ dbName } ON PORT ${ dbPort }`);
+})
+.catch(err => {
+    console.log(err);
+});
+
+const db = mongoose.connection;
+db.on('error', err => {
+    console.log(err);
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
@@ -9,6 +28,6 @@ app.get('/', (req, res) => {
     res.render('index');
 });
 
-app.listen(3000, () => {
-    console.log('SERVING ON PORT 3000');
+app.listen(serverPort, () => {
+    console.log(`SERVING ON PORT ${ serverPort }`);
 });
