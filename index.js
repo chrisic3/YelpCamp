2a9ac7726d9e69060544e010043db99ddb717a0d
chris_ic3 <chris_ic3@hotmail.com>
2026-01-12 21:08:26 -0600

    Add View and Routes to Edit a Campground Using PUT

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
+const methodOverride = require('method-override');
 const Campground = require('./models/campground');
 const campground = require('./models/campground');
 
@@ -27,6 +28,7 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride('_method'));
 
 // Home page
 app.get('/', (req, res) => {
@@ -39,7 +41,7 @@ app.get('/campgrounds', async (req, res) => {
     res.render('campgrounds/index', { campgrounds });
 });
 
-// New Campground route
+// New Campground routes
 app.get('/campgrounds/new', (req, res) => {
     res.render('campgrounds/new');
 });
@@ -55,6 +57,19 @@ app.get('/campgrounds/:id', async (req, res) => {
     const { id } = req.params;
     const campground = await Campground.findById(id);
     res.render('campgrounds/show', { campground });
+});
+
+// Edit campground routes
+app.get('/campgrounds/:id/edit', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findById(id);
+    res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+    const { id } = req.params;
+    const campground = await Campground.findByIdAndUpdate(id, { ...req.body.campground });
+    res.redirect(`/campgrounds/${ campground._id }`);
 });
 
 app.listen(serverPort, () => {
