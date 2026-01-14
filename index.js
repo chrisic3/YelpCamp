3b6419091e4e988d4edaa083356b4cd2c5af0412
chris_ic3 <chris_ic3@hotmail.com>
2026-01-13 20:01:56 -0600

    Add EJS-Mate for Templating the Views

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const mongoose = require('mongoose');
 const methodOverride = require('method-override');
+const ejsMate = require('ejs-mate');
 const Campground = require('./models/campground');
 const campground = require('./models/campground');
 
@@ -25,6 +26,7 @@ db.on('error', err => {
 const app = express();
 
 app.set('view engine', 'ejs');
+app.engine('ejs', ejsMate);
 app.set('views', path.join(__dirname, 'views'));
 
 app.use(express.urlencoded({ extended: true }));
