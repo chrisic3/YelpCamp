a1cbad24c499d2c5799a9a838b117a68b9fb8eaa
chris_ic3 <chris_ic3@hotmail.com>
2026-01-17 19:31:11 -0600

    Update the Campground Model with Image and Description
    Using Lorem Picsum for random image placeholders.

--- dbSeeds/index.js
+++ dbSeeds/index.js
@@ -27,9 +27,13 @@ const seedDB = async () => {
     for (let i = 0; i < 50; i++) {
         // The cities array has 1000 cities
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const newCampground = new Campground({
             title: `${ titleSample(descriptors) } ${ titleSample(places) }`,
-            location: `${ cities[random1000].city }, ${ cities[random1000].state }`
+            location: `${ cities[random1000].city }, ${ cities[random1000].state }`,
+            image: 'https://picsum.photos/400',
+            price: price,
+            description: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore culpa quod cum exercitationem. Voluptatem, ad voluptatum ratione deleniti reiciendis obcaecati molestias, cumque odit asperiores debitis minima hic doloremque ea est.'
         });
 
         await newCampground.save();
